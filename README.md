7ad45405b516794bd4c6ceb93b531ec62c4d99b9
imaginationcoder <labs@maisasolutions.com>
2016-03-01 15:17:57 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+> An [AWS Lambda](http://aws.amazon.com/lambda/) function that resizes videos and outputs thumbnails using [FFmpeg](https://www.ffmpeg.org/). This function is meant for short-duration videos. If you need to transcode long videos, check out [AWS Elastic Transcoder](http://aws.amazon.com/elastictranscoder/).
+
+[![Dependency Status](https://david-dm.org/binoculars/aws-lambda-ffmpeg.svg)](https://david-dm.org/binoculars/aws-lambda-ffmpeg)
+[![devDependency Status](https://david-dm.org/binoculars/aws-lambda-ffmpeg/dev-status.svg)](https://david-dm.org/binoculars/aws-lambda-ffmpeg#info=devDependencies)
+
+## Setup
+1. Install node.js, preferably through [nvm](/creationix/nvm). Lambda uses a [specific version of node](http://docs.aws.amazon.com/lambda/latest/dg/current-supported-versions.html), so it would be best use the same version--especially when installing dependencies via npm.
+1. Clone this repo
+1. Run `npm install`
+1. Create a config.json file (see below), or modify the index.js file for your purposes
+1. Create your function code's S3 bucket (this will hold your zipped lambda function) (optional if you already have a bucket for this)
+1. Run Gulp (see below)
+1. Create and upload your Lambda function
+1. Invoke the lambda function by uploading a video to your source bucket or run it manually in the AWS console.
+
+## Gulp
+See Adam Neary's ([@adamrneary](https://github.com/adamrneary)) [A Gulp Workflow for Amazon Lambda](https://medium.com/@AdamRNeary/a-gulp-workflow-for-amazon-lambda-61c2afd723b6)
+
+There are a few more Gulp tasks to download, extract, and copy the 64-bit linux build of FFmpeg from John Van Sickle's [FFmpeg builds site](http://johnvansickle.com/ffmpeg/).
+
+The default gulp task will run all of the tasks. This will result in the following actions:
+- Cleans the build directory, dist directory, and dist.zip
+- Downloads, extracts, and copies FFmpeg and FFprobe to the dist directory
+- Copies the index.js and config.json file to the dist directory
+- Runs npm install in the dist directory (excludes dev dependencies)
+- Zips everything into dist.zip. This file is what you will upload to AWS Lambda for execution.
+- Uploads the function to AWS (must create config.json first and have your AWS credentials set as environment variables).
+- Creates or updates the CloudFormation stack which includes:
+ - The lambda function's execution role and policy
+ - The lambda function
+ - The source bucket (where videos are uploaded to), including the notification configuration
+ - The destination bucket (where videos and thumbnails go after they are processed)
+
+## Lambda Function Process Overview
+1. User uploads a video file to the source bucket
+1. A notification event trigger the lambda function
+1. The lambda function downloads the video file from the source bucket
+1. Streams it through FFmpeg
+1. Outputs a scaled video file and a thumbnail image
+1. Uploads both files to the destination bucket
+
+## Configuration
+See [config_sample.json](config_sample.json). config.json will be copied after npm install.
+
+At minimum, you need to modify:
+- `functionBucket` - The name of the bucket where your the lambda function code will be uploaded to. It's necessary for CloudFormation.
+- `sourceBucket` - The name of the bucket that will receive the videos and send them to the lambda for processing.
+- `destinationBucket` - The name of the bucket that will be used to store the output video and thumnail image.
+
+## Testing
+Edit test_event.json, by modifying the bucket name and object key, and run `npm test` if you want to run a test locally.
+
+## Updates/Modification to the lambda function or configuration
+Run `gulp update` after modifying anything in the Lambda function if you've already created the stack. This will rebuild `dist.zip`, upload it to S3, and update the lambda function created during the CloudFormation stack creation.
+
+## Gotchas
+- Gzipping videos will cause Safari errors in playback. Don't enable gzip unless you don't care about supporting Safari.
+- The object key from the event is URL encoded. Spaces in the filenames might be replaced with `+` so be aware of this and handle errors appropriately. If you try to download the file with the AWS SDK for JavaScript like in this example, without handling this, it will throw an error.
+- Not handling errors with `context.fail(error)` will cause the function to run until the timeout is reached.
+- Pick the largest memory allocation. This is mostly CPU bound, but Lambda bundles memory and CPU allocation together. Memory size is 1536 by default. Testing with different videos and sizes should give you a good idea if it meets the requirements. Total execution time is limited!
+
+## Contributing
+Submit issues if you find bugs or something is unclear. Pull requests are even better, especially if you can make something more generalized.
+
+**If you use it, star it!**
---
+++ gulpfile.js
@@ -0,0 +1,204 @@
+var http = require('http');
+var fs = require('fs');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var shell = require('gulp-shell');
+var flatten = require('gulp-flatten');
+var rename = require('gulp-rename');
+var del = require('del');
+var install = require('gulp-install');
+var zip = require('gulp-zip');
+var AWS = require('aws-sdk');
+var runSequence = require('run-sequence');
+var async = require('async');
+var s3 = new AWS.S3();
+var lambda = new AWS.Lambda();
+
+var config;
+try {
+	config = require('./config.json');
+} catch (ex) {
+	config = {};
+}
+
+var filename = './build/ffmpeg-git-64bit-static.tar.xz';
+var fileURL = 'http://johnvansickle.com/ffmpeg/builds/ffmpeg-git-64bit-static.tar.xz';
+
+gulp.task('postinstall', function(cb) {
+	async.reject(
+		['config.json', 'test_event.json'],
+		fs.exists,
+		function(files) {
+			async.map(files, function(file, cb) {
+				return cb(null, gulp.src(file.replace(/\.json/, '_sample.json'))
+						.pipe(rename(file))
+						.pipe(gulp.dest('.'))
+				);
+			}, cb);
+		}
+	);
+});
+
+gulp.task('download-ffmpeg', function(cb) {
+	if(!fs.existsSync('./build')) {
+		fs.mkdirSync('./build');
+	}
+
+	var file = fs.createWriteStream(filename);
+	http.get(fileURL, function(response) {
+		response.pipe(file);
+
+		file.on('finish', function() {
+			file.close();
+			cb();
+		})
+	});
+});
+
+// Resorting to using a shell task. Tried a number of other things including
+// LZMA-native, node-xz, decompress-tarxz. None of them work very well with this.
+// This will probably work well for OS X and Linux, but maybe not Windows without Cygwin.
+gulp.task('untar-ffmpeg', shell.task([
+	'tar -xvf ' + filename + ' -C ./build'
+]));
+
+gulp.task('copy-ffmpeg', function() {
+	return gulp.src(['build/ffmpeg-*/ffmpeg', 'build/ffmpeg-*/ffprobe'])
+		.pipe(flatten())
+		.pipe(gulp.dest('./dist'));
+});
+
+/*
+ From: https://medium.com/@AdamRNeary/a-gulp-workflow-for-amazon-lambda-61c2afd723b6
+ */
+
+// First we need to clean out the dist folder and remove the compiled zip file.
+gulp.task('clean', function() {
+	return del([
+		'./build/*',
+		'./dist/*',
+		'./dist.zip'
+	]);
+});
+
+// The js task could be replaced with gulp-coffee as desired.
+gulp.task('js', function() {
+	return gulp.src(['index.js', 'config.json'])
+		.pipe(gulp.dest('./dist'))
+});
+
+// Here we want to install npm packages to dist, ignoring devDependencies.
+gulp.task('npm', function() {
+	return gulp.src('./package.json')
+		.pipe(gulp.dest('./dist'))
+		.pipe(install({production: true}));
+});
+
+// Now the dist directory is ready to go. Zip it.
+gulp.task('zip', function() {
+	return gulp.src(['dist/**/*', '!dist/package.json', 'dist/.*'])
+		.pipe(zip('dist.zip'))
+		.pipe(gulp.dest('./'));
+});
+
+var cloudFormation = new AWS.CloudFormation();
+var packageInfo = require('./package.json');
+var bucket = config.functionBucket;
+var functionName = packageInfo.name;
+var key = functionName + '.zip';
+
+// Upload the function code to S3
+gulp.task('upload', function(cb) {
+	s3.upload({
+		Bucket: bucket,
+		Key: key,
+		Body: fs.createReadStream('./dist.zip')
+	}, cb);
+});
+
+var stackName = packageInfo.name;
+
+// Deploy the CloudFormation Stack
+gulp.task('deployStack', function(cb) {
+	cloudFormation.describeStacks({
+		StackName: stackName
+	}, function(err) {
+		var operation = err ? 'createStack' : 'updateStack';
+
+		cloudFormation[operation]({
+			StackName: stackName,
+			Capabilities: [
+				'CAPABILITY_IAM'
+			],
+			Parameters: [
+				{
+					ParameterKey: 'SourceBucketName',
+					ParameterValue: config.sourceBucket
+				},
+				{
+					ParameterKey: 'DestinationBucketName',
+					ParameterValue: config.destinationBucket
+				},
+				{
+					ParameterKey: 'LambdaS3Bucket',
+					ParameterValue: bucket
+				},
+				{
+					ParameterKey: 'LambdaS3Key',
+					ParameterValue: key
+				}
+			],
+			TemplateBody: fs.readFileSync('./cloudformation.json', {encoding: 'utf8'})
+		}, cb);
+	});
+});
+
+// Once the stack is deployed, this will update the function if the code is changed without recreating the stack
+gulp.task('updateCode', function(cb) {
+	async.waterfall([
+		function(cb) {
+			cloudFormation.describeStackResource({
+				StackName: stackName,
+				LogicalResourceId: 'Lambda'
+			}, cb);
+		},
+		function(data, cb) {
+			lambda.updateFunctionCode({
+				FunctionName: data.StackResourceDetail.PhysicalResourceId,
+				S3Bucket: bucket,
+				S3Key: key
+			}, cb);
+		}
+	], cb);
+});
+
+// Builds the function and uploads
+gulp.task('build-upload', function(cb) {
+	return runSequence(
+		'clean',
+		'download-ffmpeg',
+		'untar-ffmpeg',
+		['copy-ffmpeg', 'js', 'npm'],
+		'zip',
+		'upload',
+		cb
+	);
+});
+
+// For an already created stack
+gulp.task('update', function(cb) {
+	return runSequence(
+		'build-upload',
+		'updateCode',
+		cb
+	);
+});
+
+// For a new stack (or you change cloudformation.json)
+gulp.task('default', function(cb) {
+	return runSequence(
+		'build-upload',
+		'deployStack',
+		cb
+	);
+});
